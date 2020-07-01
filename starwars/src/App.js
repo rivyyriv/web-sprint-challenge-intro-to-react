b306a6d60e514f6505960d37863d5153b30926b2
River Smith <rivyyriv@gmail.com>
2020-07-01 15:52:26 -0600

    successfully maps over data and renders to the screen

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import PlanetCard from './Planet.js';
+
 
 
 const App = () => {
 
-  let planetcards = [];
- 
-  axios.get('https://swapi.dev/api/planets/')
+  const [resourcetype, setResourse] = useState('planets/')
+  const [items, setItems] = useState([])
 
-        .then( planets => {
-          
-           planets.data.results.forEach(element => {
-
-              planetcards.push(element);
-
-            });
-
-        })
-
-        .catch( error => {
-            console.log('Stuck in catch' + error)
-        })
-
-
-
-  return (
+  useEffect(() => {
     
-    <PlanetCard planetinfo={planetcards}/>
+    axios.get(`https://swapi.dev/api/${resourcetype}`)
+    .then(response => setItems(response.data.results))
+    .catch()
+    
+  }, [resourcetype])
 
-  );
+  console.log(items)
+
+  return(
+  <>
+  <div>
+    <button onClick={ () => setResourse('planets/')}>Planets</button>
+    <button onClick={ () => setResourse('people/')}>People</button>
+    <button onClick={ () => setResourse('starships/')}>Starships</button>
+    <h1>{resourcetype}</h1>
+  </div>
+  {items.map( item => {
+    return <pre>{JSON.stringify(item)}</pre>
+  })}
+
+ 
+  </>
+  )
+
 }
+
 
 export default App;
